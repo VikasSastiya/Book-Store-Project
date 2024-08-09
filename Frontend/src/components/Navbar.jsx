1960f487b4ab76facab3f9facad66a25c02ea6cf
VikasSastiya <vikassastiya06@gmail.com>
2024-08-09 20:40:43 +0530

    navbar2

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 function Navbar() {
+    const navItems= (
+      <>
+       <li><a>Home</a></li>
+      <li><a>Course</a></li>
+      <li><a>Contact</a></li>
+      <li><a>About</a></li>
+      </>
+    );
   return (
     <>
-   <div>
+   <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
    <div className="navbar bg-base-100">
   <div className="navbar-start">
     <div className="dropdown">
@@ -23,49 +31,37 @@ function Navbar() {
       </div>
       <ul
         tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-          
-          <li>
-        <a>Home</a>
-        </li>
-        <li>
-        <a>Course</a>
-        </li>
-        <li>
-        <a>Contact</a>
-        </li>
-        <li>
-        <a>About</a>
-        </li>
-        
-      </ul>
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">{navItems} </ul>
     </div>
     <a className="text-2xl font-bold cursor-pointer">BookStore</a>
   </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-       <li>
-        <a>Home</a>
-        </li>
-        <li>
-        <a>Course</a>
-        </li>
-        <li>
-        <a>Contact</a>
-        </li>
-        <li>
-        <a>About</a>
-        </li>
-     </ul>
-  </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">{navItems}</ul>
   </div>
+  <div className="hidden md:block">
+  <label className="input input-bordered flex items-center gap-2">
+  <input type="text" className="grow" placeholder="Search" />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    fill="currentColor"
+    className="h-4 w-4 opacity-70">
+    <path
+      fillRule="evenodd"
+      d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+      clipRule="evenodd" />
+  </svg>
+</label>
+  </div>
+  <div className="">
+    <a className="bg-white text-black p-2 rounded-md  duration-300">Login</a>
+  </div>
+</div>
 </div>
 </div>
     </>
   )
 }
-
 
 export default Navbar
